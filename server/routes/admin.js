1937f894596bf638d52ca339a391bbd367fdf105
Junaid Sarwar <junaidsarwariqbal@gmail.com>
2024-12-27 22:31:14 +0500

    done for Lectures.js

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { isAdmin, isAuth } from '../middleware/isAuth.js';
-import { createCourse } from '../controllers/admin.js';
+import { addLecture, createCourse } from '../controllers/admin.js';
 import { uploadFiles } from '../middleware/multer.js';
 
 const router = express.Router();
 
-router.post('/course/new',isAuth, isAdmin , uploadFiles ,createCourse)
+router.post('/course/new',isAuth, isAdmin , uploadFiles ,createCourse);
+router.post('/course/:id',isAuth,isAdmin,uploadFiles,addLecture)
 
 export default router
